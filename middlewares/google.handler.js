ff83130c1a7e99cbda8364a064adaeb9c7740d3e
Santiago Restrepo <santirstrp@gmail.com>
2023-04-24 07:45:55 -0500

    change routes

--- middlewares/google.handler.js
+++ middlewares/google.handler.js
@@ -7,7 +7,7 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/api/auth/google/callback"
+    callbackURL: "https://uclass-backend.vercel.app/api/auth/google/callback"
     },
     async function(accessToken, refreshToken, profile, done) {
         const googleId = profile.id;
