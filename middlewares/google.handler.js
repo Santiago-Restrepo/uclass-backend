edff53e09ff2894ce4dcbb2b763c13222d3f9a38
Santiago Restrepo <santirstrp@gmail.com>
2023-05-09 21:25:08 -0500

    add GOOGLE_CALLBACK_URL as env variable

--- middlewares/google.handler.js
+++ middlewares/google.handler.js
@@ -8,7 +8,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "https://uclass-backend.vercel.app/api/auth/google/callback"
-    callbackURL: "http://localhost:3000/api/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
     async function(accessToken, refreshToken, profile, done) {
         const googleId = profile.id;
